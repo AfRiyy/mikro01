68a3a710deb20e77cc747cf61880194f817ba8fa
AfRiyy <AfRiyy@gmail.com>
2021-10-14 10:44:23 +0200

    mentés

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const {src,dest} = require('gulp');
+const ts = require('gulp-typescript');
+const { watch } = require('gulp');
+const {parallel} = require('gulp');
+
+function streamTS() {
+    return src('src/**/*.ts')
+    .pipe(ts()).js
+    .pipe(dest('public'));
+}
+
+function streamHtml(){
+    return src('src/*.html')
+    .pipe(dest('public'));
+}
+
+exports.default = parallel(streamHtml,streamTS);
+exports.server = streamHtml;
